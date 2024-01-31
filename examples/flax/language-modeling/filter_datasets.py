01fb37ef138f7b773650861ec923e3ff07a44532
Damith Premasiri <damithpremasiri@gmail.com>
2024-01-31 16:10:23 +0000

    dataset filter files

--- examples/flax/language-modeling/filter_datasets.py
+++ examples/flax/language-modeling/filter_datasets.py
@@ -3,6 +3,8 @@ import argparse
 import pandas as pd
 from datasets import load_dataset
 
+from sklearn.model_selection import train_test_split
+
 parser = argparse.ArgumentParser(description='''convert dataset''')
 parser.add_argument('--cache_dir', type=str, default=None, required=False,
                     help='model_name')
@@ -35,6 +37,16 @@ df = pd.DataFrame()
 df['text'] = filtered_list
 df.to_csv(f'data_files/en_all_filtered_1024_part_{part_number}.tsv', sep='\t', index=False)
 print(f'used count {count} | removed count {removed}')
+
+files_list = [f'en_all_filtered_1024_part_{i}.tsv' for i in range(1, 18)]
+dataframes = [pd.read_csv(f'data_files/{file}', sep='\t') for file in files_list]
+df = pd.concat(dataframes, ignore_index=True)
+df = df.dropna(how='all')
+
+train, validation = train_test_split(df, test_size=0.1, random_state=777, shuffle=True)
+
+train.to_csv('training_file.tsv', sep='\t', index=False)
+validation.to_csv('validation_file.tsv', sep='\t', index=False)
 
 # used count 8377505 | removed count 9859559 - missed last part
 # used count 8377505 | removed count 9859559 - add them together to get total
